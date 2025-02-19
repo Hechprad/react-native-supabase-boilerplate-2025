d70aa02c38eb1fa2593d039a7eaa47c8244216c1
Hecherat <hecherat@gmail.com>
2025-02-19 13:44:31 -0300

    feat: add useCheckForAppUpdate hook

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,14 +1,17 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
 export default function NotFoundScreen() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text>This screen doesn't exist.</Text>
+        <Text>{t('pages.notFound.title')}</Text>
         <Link href="/dashboard/page" style={styles.link}>
-          <Text>Go to home screen!</Text>
+          <Text>{t('pages.notFound.link')}</Text>
         </Link>
       </View>
     </>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,22 @@
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
 
 import { setZodCustomErrorMap } from '@/lib/zod';
+// import { Redirect } from 'expo-router';
 
 import colors from '@/constants/theme/colors';
+// import useCheckForAppUpdate from '@/hooks/useCheckForAppUpdate';
 
 setZodCustomErrorMap();
 
 // Initial loading screen
 export default function Index() {
+  // @TODO - Ativar quando o app estiver publicado
+  // const hasUpdate = useCheckForAppUpdate();
+
+  // if (hasUpdate) {
+  //   return <Redirect href="/update" />;
+  // }
+
   return (
     <View style={indexStyles.container}>
       <ActivityIndicator size={44} color={colors.palette.primary.mintGreen} />
